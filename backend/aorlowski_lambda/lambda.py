c4f62bf75ad92307558e1853f7a781186b76cb8e
Andrew <andyremigaillard@gmail.com>
2022-09-25 15:31:50 -0500

    Improve comments

--- backend/aorlowski_lambda/lambda.py
+++ backend/aorlowski_lambda/lambda.py
@@ -4,6 +4,8 @@ east_1_session = boto3.Session(region_name = 'us-east-1')
 table = east_1_session.resource('dynamodb').Table('aorlowski-visitors')
 def lambda_handler(event, context):
     response_body_count = 0
+
+    # If it's a POST, we increment the counter & return the incremented value
     if (event['httpMethod'] == 'POST'):
         # Increment the visitor counter atomically
         update = table.update_item(
@@ -18,13 +20,14 @@ def lambda_handler(event, context):
         )
         
         response_body_count = update['Attributes']['Quantity']
+
+    # Anything other than a POST and we return the value without incrementing
     else:
         response = table.get_item(Key={'statistic': 'view-count'})
         item = response['Item']
         count = item['Quantity']
         response_body_count = count
 
-    #TODO implement
     result = {
         "isBase64Encoded": False,
         "statusCode": "200",
