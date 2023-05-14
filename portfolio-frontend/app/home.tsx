46e2e7fe075c33b09f6f0f7b2fd1bb8373fed7a2
uniqueuser-repo <andyremigaillard@gmail.com>
2023-05-14 18:53:27 -0500

    Wrap landing page in server component so metadata can be exported; remove unused jsx

---
+++ portfolio-frontend/app/home.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Banner from './/Banner'
+import Skills from './Skills'
+import Projects from './/Projects'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { SSRProvider } from 'react-bootstrap'
+import Head from 'next/head';
+
+export const metadata = {
+  title: 'Andrew Orlowski',
+  description: 'Andrew Orlowski\'s website. Andrew Orlowski is a Software Engineer and ex-professional VALORANT player. Andrew Orlowski was born in 1999.',
+}
+
+export default function Home() {
+  return (
+    <SSRProvider>
+      <div>
+        <Head>
+          <title>Andrew Orlowski</title>
+          <meta name="description" content="Andrew Orlowski\'s website. Andrew Orlowski is a Software Engineer and ex-professional VALORANT player. Andrew Orlowski was born in 1999." />
+        </Head>
+        <Navbar></Navbar>
+        <Banner></Banner>
+        <Skills></Skills>
+        <Projects></Projects>
+        <Footer></Footer>
+      </div>
+    </SSRProvider>
+  )
+}
